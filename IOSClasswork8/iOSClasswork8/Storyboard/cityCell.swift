c74df1d6413bab9420e8524d860c9d37d25f22de
ali3dashti <aadashti73@gmail.com>
2020-07-08 21:06:23 +0300

    تم الحل

---
+++ IOSClasswork8/iOSClasswork8/Storyboard/cityCell.swift
@@ -0,0 +1,17 @@
+//
+//  cityCell.swift
+//  iOSClaswork8
+//
+//  Created by Ali Dashti on 7/8/20.
+//  Copyright © 2020 Safeyah Coding. All rights reserved.
+//
+
+import UIKit
+
+class cityCell: UITableViewCell {
+
+    @IBOutlet weak var cardLabel: UILabel!
+    
+    @IBOutlet weak var cardImage: UIImageView!
+    
+}
